7b8161b45cde4bf6a5d342b9acab169d655b1e84
decongo <conger.devon@gmail.com>
2021-03-21 13:11:49 -0600

    add project files

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello_world.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<HelloWorld>/
+        public IActionResult Index(string userInput = "World")
+        {
+            ViewData["userInput"] = userInput + "!";
+
+            return View();
+        }
+    }
+}
